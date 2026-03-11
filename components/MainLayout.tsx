e9d275b772c84873a692dba1054a0a83f8b199bb
Gwanghee Jo <92922343+Jojorabbit21@users.noreply.github.com>
2026-03-11 22:00:10 +0900

    Update MainLayout.tsx

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -105,7 +105,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children, sidebarProps, gameHea
     }, [team, teams, schedule]);
 
     const isFullHeightView = sidebarProps.currentView === 'DraftRoom' || sidebarProps.currentView === 'DraftHistory' || sidebarProps.currentView === 'DraftLottery';
-    const isNoPaddingView = sidebarProps.currentView === 'Dashboard' || sidebarProps.currentView === 'Inbox' || sidebarProps.currentView === 'Roster' || sidebarProps.currentView === 'Standings' || sidebarProps.currentView === 'Leaderboard' || sidebarProps.currentView === 'Schedule' || sidebarProps.currentView === 'Transactions' || sidebarProps.currentView === 'PlayerDetail' || sidebarProps.currentView === 'Playoffs';
+    const isNoPaddingView = sidebarProps.currentView === 'Dashboard' || sidebarProps.currentView === 'Inbox' || sidebarProps.currentView === 'Roster' || sidebarProps.currentView === 'Standings' || sidebarProps.currentView === 'Leaderboard' || sidebarProps.currentView === 'Schedule' || sidebarProps.currentView === 'Transactions' || sidebarProps.currentView === 'PlayerDetail' || sidebarProps.currentView === 'CoachDetail' || sidebarProps.currentView === 'Playoffs';
 
     return (
         <div className="flex h-screen bg-slate-950 overflow-hidden text-slate-200 selection:bg-indigo-500/30">
